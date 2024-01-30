d30efd7683ce67ec8e443bb2e70ed31c5e77d9b8
Mima9926 <mikechapuma@gmail.com>
2024-01-30 23:58:20 +0200

    managment command

---
+++ msestocks/management/commands/update_stock_data.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+from msestocks.views import update_stock_data
+
+class Command(BaseCommand):
+    help = 'Updates stock data'
+
+    def handle(self, *args, **options):
+        update_stock_data()
