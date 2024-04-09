c561eb6622891caf1a9107f6cbbebcb2530d56bf
Mima9926 <mikechapuma@gmail.com>
2024-04-09 04:27:48 +0200

    css linkage

--- stats265/settings.py
+++ stats265/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,7 +117,9 @@ TIME_ZONE = 'Africa/Blantyre'
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+
+STATIC_ROOT = BASE_DIR / 'msestocks/static/msestocks'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
